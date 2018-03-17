94a0ec311f1c4bdbd726e22a7b3f84a1805a2d1f
niks8693 <nikhil.shende2018@gmail.com>
2018-03-17 13:37:19 -0500

    used feign as REST client for service invocation

--- src/main/java/com/niks/springcloud/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/com/niks/springcloud/currencyconversionservice/CurrencyConversionController.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,8 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
+	@Autowired
+	private CurrencyExchangeServiceProxy proxy;
 
 	@GetMapping("currency-converter/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to,
@@ -23,6 +26,17 @@ public class CurrencyConversionController {
 				"http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversionBean.class,
 				uriVariables);
 		CurrencyConversionBean response = responseEntity.getBody();
+		return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity,
+				quantity.multiply(response.getConversionMultiple()), response.getPort());
+
+	}
+
+	@GetMapping("currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversionBean convertCurrencyfeign(@PathVariable String from, @PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+		
+		//using feign
+		CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
 		return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity,
 				quantity.multiply(response.getConversionMultiple()), response.getPort());
 
--- src/main/java/com/niks/springcloud/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/niks/springcloud/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,8 +2,10 @@ package com.niks.springcloud.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.niks.springcloud.currencyconversionservice")
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
